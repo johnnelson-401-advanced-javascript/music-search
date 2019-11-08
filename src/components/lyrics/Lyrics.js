f3adbb423e6a7447724b3013fdddd4e78d33abd4
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-08 11:31:12 -0800

    CSS tweaks

--- src/components/lyrics/Lyrics.js
+++ src/components/lyrics/Lyrics.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Lyrics.css';
 
 export default function Lyrics({ title, artist, lyrics }) {
   return (
     <>
-      <h3>{`${title} by ${artist}`}</h3>
-      <p>
-        <span>{lyrics}</span>
-      </p>
+      <span>
+        <h3>{`${title} by ${artist}`}</h3>
+        <p className={styles.Lyrics}>
+          <span>{lyrics}</span>
+        </p>
+      </span>
     </>
   );
 }
